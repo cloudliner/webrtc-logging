1273b68f78b836a988a3ca41198ec4dbbcadca25
cloudliner <cloudliner.jp@gmail.com>
2020-01-27 12:13:47 +0900

    minor fix

--- src/app/info/EventInfo.ts
+++ src/app/info/EventInfo.ts
@@ -1,9 +1,9 @@
 export class EventInfo {
 
   constructor(private event: Event) {}
-
   getFields(): string {
-    return `type=${ this.event.type }, target=${ event.target }, srcElement=${ event.srcElement }, eventPhase=${ event.eventPhase }`;
+    // tslint:disable-next-line: max-line-length
+    return `type=${ this.event.type }, target=${ this.event.target }, currentTarget=${ this.event.currentTarget }, eventPhase=${ this.event.eventPhase }`;
   }
   toString(): string {
     return `{ ${ this.getFields() } }`;
@@ -15,7 +15,6 @@ export class MediaStreamTrackEventInfo extends EventInfo {
   constructor(private mediaStreamTrackEvent: MediaStreamTrackEvent) {
     super(mediaStreamTrackEvent);
   }
-
   getFields(): string {
     return super.getFields() + `, track=${ this.mediaStreamTrackEvent.track }`;
   }
